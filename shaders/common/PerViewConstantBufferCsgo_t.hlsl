9603e9725ff7932d1d1825eba0516a0dddbdb8be
kristiker <kristik06@outlook.com>
2023-11-01 11:13:50 +0100

    Split fogging, cleanup pixelshader

--- shaders/common/PerViewConstantBufferCsgo_t.hlsl
+++ shaders/common/PerViewConstantBufferCsgo_t.hlsl
@@ -3,6 +3,8 @@ cbuffer PerViewConstantBufferCsgo_t : register(b18, space0)
     bool4 g_bFogTypeEnabled;
     bool4 g_bOtherFxEnabled;
     bool4 g_bOtherEnabled2;
+
+    // Bindless texture indices
     uint g_tBlueNoiseTextureIndex;
     uint g_tBRDFLookupTextureIndex;
     uint g_tCubemapFogTextureIndex;
@@ -14,6 +16,7 @@ cbuffer PerViewConstantBufferCsgo_t : register(b18, space0)
     uint g_tMomentsIndex;
     uint g_tExtra_MomentIndex;
     uint g_tLowShaderQualityFallbackCubemap;
+
     uint g_tUnused0;
     float2 g_vBlueNoiseMask;
     uint g_tUnused1;
@@ -28,15 +31,15 @@ cbuffer PerViewConstantBufferCsgo_t : register(b18, space0)
     float4 g_vInteractionVolumeInvExtents;
     float4 g_vInteractionTriggerVolumeInvMins;
     float4 g_vInteractionTriggerVolumeWorldToVolumeScale;
-    float4 g_vGradientFogBiasAndScale;
-    float4 m_vGradientFogExponents;
-    float4 g_vGradientFogColor_Opacity;
-    float4 g_vGradientFogCullingParams;
-    float4 g_vCubeFog_Offset_Scale_Bias_Exponent;
-    float4 g_vCubeFog_Height_Offset_Scale_Exponent_Log2Mip;
-    column_major float4x4 g_matvCubeFogSkyWsToOs;
-    float4 g_vCubeFogCullingParams_MaxOpacity;
-    float4 g_vCubeFog_ExposureBias;
+    float4 g_vGradientFogBiasAndScale : packoffset(c18);
+    float4 m_vGradientFogExponents : packoffset(c19);
+    float4 g_vGradientFogColor_Opacity : packoffset(c20);
+    float4 g_vGradientFogCullingParams : packoffset(c21);
+    float4 g_vCubeFog_Offset_Scale_Bias_Exponent : packoffset(c22);
+    float4 g_vCubeFog_Height_Offset_Scale_Exponent_Log2Mip : packoffset(c23);
+    column_major float4x4 g_matvCubeFogSkyWsToOs : packoffset(c24);
+    float4 g_vCubeFogCullingParams_MaxOpacity : packoffset(c28);
+    float4 g_vCubeFog_ExposureBias : packoffset(c29);
     float4 g_vHighPrecisionLightingOffsetWs : packoffset(c30);
     float g_flEnvMapPositionBias;
     float g_flScopeMagnification;
